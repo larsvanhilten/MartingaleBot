37759105fabb92e233e7d624a9e62caa5e886ff8
Lars van Hilten <larsvh8@gmail.com>
2016-04-30 19:41:03 +0200

    1.0

---
+++ MartingaleBot/MainWindow.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MartingaleBot
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new Gecko.GeckoWebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colourCombo = new System.Windows.Forms.ComboBox();
+            this.startCreditsLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startAmountTextbox = new System.Windows.Forms.MaskedTextBox();
+            this.concurWinLabel = new System.Windows.Forms.Label();
+            this.concurLossLabel = new System.Windows.Forms.Label();
+            this.winsLabel = new System.Windows.Forms.Label();
+            this.creditsLabel = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopCheckbox = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Location = new System.Drawing.Point(378, 12);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(394, 388);
+            this.browser.TabIndex = 0;
+            this.browser.UseHttpActivityObserver = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.colourCombo);
+            this.groupBox1.Controls.Add(this.startCreditsLabel);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.startAmountTextbox);
+            this.groupBox1.Controls.Add(this.concurWinLabel);
+            this.groupBox1.Controls.Add(this.concurLossLabel);
+            this.groupBox1.Controls.Add(this.winsLabel);
+            this.groupBox1.Controls.Add(this.creditsLabel);
+            this.groupBox1.Controls.Add(this.userLabel);
+            this.groupBox1.Controls.Add(this.startButton);
+            this.groupBox1.Controls.Add(this.stopCheckbox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 388);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Colour";
+            // 
+            // colourCombo
+            // 
+            this.colourCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.colourCombo.FormattingEnabled = true;
+            this.colourCombo.Items.AddRange(new object[] {
+            "red",
+            "black"});
+            this.colourCombo.Location = new System.Drawing.Point(279, 256);
+            this.colourCombo.Name = "colourCombo";
+            this.colourCombo.Size = new System.Drawing.Size(75, 21);
+            this.colourCombo.TabIndex = 10;
+            // 
+            // startCreditsLabel
+            // 
+            this.startCreditsLabel.AutoSize = true;
+            this.startCreditsLabel.Location = new System.Drawing.Point(6, 79);
+            this.startCreditsLabel.Name = "startCreditsLabel";
+            this.startCreditsLabel.Size = new System.Drawing.Size(80, 13);
+            this.startCreditsLabel.TabIndex = 9;
+            this.startCreditsLabel.Text = "Credits on start:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Start amount";
+            // 
+            // startAmountTextbox
+            // 
+            this.startAmountTextbox.Location = new System.Drawing.Point(279, 324);
+            this.startAmountTextbox.Name = "startAmountTextbox";
+            this.startAmountTextbox.Size = new System.Drawing.Size(75, 20);
+            this.startAmountTextbox.TabIndex = 7;
+            this.startAmountTextbox.Text = "1";
+            // 
+            // concurWinLabel
+            // 
+            this.concurWinLabel.AutoSize = true;
+            this.concurWinLabel.Location = new System.Drawing.Point(6, 115);
+            this.concurWinLabel.Name = "concurWinLabel";
+            this.concurWinLabel.Size = new System.Drawing.Size(89, 13);
+            this.concurWinLabel.TabIndex = 6;
+            this.concurWinLabel.Text = "Concurrent Wins:";
+            // 
+            // concurLossLabel
+            // 
+            this.concurLossLabel.AutoSize = true;
+            this.concurLossLabel.Location = new System.Drawing.Point(6, 102);
+            this.concurLossLabel.Name = "concurLossLabel";
+            this.concurLossLabel.Size = new System.Drawing.Size(94, 13);
+            this.concurLossLabel.TabIndex = 5;
+            this.concurLossLabel.Text = "Concurrent losses:";
+            // 
+            // winsLabel
+            // 
+            this.winsLabel.AutoSize = true;
+            this.winsLabel.Location = new System.Drawing.Point(6, 53);
+            this.winsLabel.Name = "winsLabel";
+            this.winsLabel.Size = new System.Drawing.Size(34, 13);
+            this.winsLabel.TabIndex = 4;
+            this.winsLabel.Text = "Wins:";
+            // 
+            // creditsLabel
+            // 
+            this.creditsLabel.AutoSize = true;
+            this.creditsLabel.Location = new System.Drawing.Point(6, 40);
+            this.creditsLabel.Name = "creditsLabel";
+            this.creditsLabel.Size = new System.Drawing.Size(42, 13);
+            this.creditsLabel.TabIndex = 3;
+            this.creditsLabel.Text = "Credits:";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(6, 27);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(35, 13);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "User: ";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(279, 359);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopCheckbox
+            // 
+            this.stopCheckbox.AutoSize = true;
+            this.stopCheckbox.Location = new System.Drawing.Point(6, 363);
+            this.stopCheckbox.Name = "stopCheckbox";
+            this.stopCheckbox.Size = new System.Drawing.Size(114, 17);
+            this.stopCheckbox.TabIndex = 0;
+            this.stopCheckbox.Text = "Stop after next win";
+            this.stopCheckbox.UseVisualStyleBackColor = true;
+            this.stopCheckbox.CheckedChanged += new System.EventHandler(this.setStopNext);
+            // 
+            // MainWindow
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.browser);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "Martingale Bot - Lemondragon";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gecko.GeckoWebBrowser browser;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox stopCheckbox;
+        private System.Windows.Forms.Label concurWinLabel;
+        private System.Windows.Forms.Label concurLossLabel;
+        private System.Windows.Forms.Label winsLabel;
+        private System.Windows.Forms.Label creditsLabel;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label startCreditsLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox startAmountTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox colourCombo;
+    }
+}
+
---
+++ MartingaleBot/MainWindow.cs
@@ -0,0 +1,306 @@
+﻿using Gecko;
+using Gecko.DOM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace MartingaleBot
+{
+    public partial class MainWindow : Form
+    {
+        private string username;
+        private int credits;
+        private int startCredits;
+        private int concurrentLosses = 0;
+        private int concurrentWins = 0;
+        private int wins = 0;
+        private int betStreak = 0;
+        private int previousBet;
+        private bool stopNext = false;
+
+        private System.Timers.Timer timer;
+
+        private GeckoElement usernameElement;
+        private GeckoElement creditElement;
+        private GeckoInputElement textboxElement;
+        private GeckoHtmlElement redButton;
+        private GeckoHtmlElement blackButton;
+        private GeckoElement resultList;
+        private GeckoElement statusText;
+        private GeckoElement betRedStatusText;
+        private GeckoElement betBlackStatusText;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            browser.Navigate("https://www.csgohill.com/");
+            colourCombo.SelectedIndex = 0;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+            int loggedIn = browser.Document.GetElementsByClassName("mobile_menu loginButton").Count();
+            if (loggedIn == 1)
+            {
+                MessageBox.Show("Please log in.");
+            }
+            else if (loggedIn == 0)
+            {
+
+                startButton.Enabled = false;
+                colourCombo.Enabled = false;
+                startAmountTextbox.Enabled = false;
+                //browser.Enabled = false;
+
+                initializeElements();
+
+                getName();
+                getCredits();
+                setStartCredits();
+
+                timer = new System.Timers.Timer(5000);
+                timer.Elapsed += logic;
+                timer.Start();
+
+            }
+
+
+        }
+
+        private void initializeElements()
+        {
+            usernameElement = (GeckoElement)browser.Document.GetElementsByClassName("username")[0];
+            creditElement = (GeckoElement)browser.Document.GetElementsByClassName("credits")[0];
+
+            GeckoElement textboxParent = (GeckoElement)browser.Document.GetElementsByClassName("bet_system")[0];
+            foreach (GeckoNode child in textboxParent.ChildNodes)
+            {
+                if (child.NodeName.ToLower() == "input")
+                {
+                    textboxElement = (GeckoInputElement)child;
+                }
+            }
+
+            GeckoNode redDiv = browser.Document.GetElementsByClassName("table")[0];
+            foreach (GeckoNode child in redDiv.ChildNodes)
+            {
+                if (NodeType.Element == child.NodeType)
+                {
+                    GeckoElement convert = (GeckoElement)child;
+                    if (convert.GetAttribute("data-color") == "red")
+                    {
+                        redButton = (GeckoHtmlElement)child;
+                        Console.WriteLine("Got Red");
+                    }
+                }
+
+            }
+
+            GeckoNode blackDiv = browser.Document.GetElementsByClassName("table")[2];
+            foreach (GeckoNode child in blackDiv.ChildNodes)
+            {
+                if (NodeType.Element == child.NodeType)
+                {
+                    GeckoElement convert = (GeckoElement)child;
+                    if (convert.GetAttribute("data-color") == "black")
+                    {
+                        blackButton = (GeckoHtmlElement)child;
+                        Console.WriteLine("Got Black");
+                    }
+                }
+
+            }
+
+            GeckoNode resultListParent = browser.Document.GetElementsByClassName("winner_history")[0];
+            foreach (GeckoNode child in resultListParent.ChildNodes)
+            {
+                if (NodeType.Element == child.NodeType)
+                {
+                    GeckoElement convert = (GeckoElement)child;
+                    if (convert.GetAttribute("class") == "holder")
+                    {
+                        resultList = (GeckoElement)child;
+                    }
+                }
+
+            }
+
+            GeckoNode statusTextParent = browser.Document.GetElementsByClassName("roll_timer")[0];
+            foreach (GeckoNode child in statusTextParent.ChildNodes)
+            {
+                if (NodeType.Element == child.NodeType)
+                {
+                    GeckoElement convert = (GeckoElement)child;
+                    if (convert.GetAttribute("class") == "bg")
+                    {
+                        statusText = (GeckoElement)child;
+                    }
+                }
+
+            }
+
+            betRedStatusText = (GeckoElement)browser.Document.GetElementsByClassName("your")[0];
+            betBlackStatusText = (GeckoElement)browser.Document.GetElementsByClassName("your")[2];
+
+
+        }
+
+        private void logic(object source, ElapsedEventArgs e)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+
+                string betText;
+
+                if (colourCombo.Text == "red")
+                {
+                    betText = betRedStatusText.TextContent;
+
+                }
+                else {
+                    betText = betBlackStatusText.TextContent;
+                }
+
+
+                String[] textParts = betText.Split(':');
+                int currentBet = Int32.Parse(textParts[1]);
+                if (currentBet == 0)
+                {
+                    if (betStreak > 0)
+                    {
+                        checkIfWon();
+                        updateLabels();
+                    }
+
+                    if (stopNext == true && betStreak == 0)
+                    {
+                        timer.Stop();
+                        updateLabels();
+                        startButton.Enabled = true;
+                        colourCombo.Enabled = true;
+                        startAmountTextbox.Enabled = true;
+                        stopCheckbox.Checked = false;
+                    }
+                    else {
+
+                        if (statusText.TextContent.Contains("Rolling in"))
+                        {
+                            bet();
+                            updateLabels();
+                        }
+                    }
+
+                }
+
+
+            }));
+
+        }
+
+        public void getName()
+        {
+            username = usernameElement.TextContent;
+            userLabel.Text = "User: " + username;
+        }
+
+        public void getCredits()
+        {
+            string creditsText = creditElement.TextContent;
+            string[] creditsSplit = creditsText.Split(':');
+            credits = Int32.Parse(creditsSplit[1]);
+            creditsLabel.Text = "Credits: " + credits;
+        }
+
+        public void setStartCredits()
+        {
+            startCredits = credits;
+            startCreditsLabel.Text = "Credits on start: " + startCredits;
+        }
+
+        public void bet()
+        {
+            int bet = 0;
+            if (betStreak == 0)
+            {
+                bet = Int32.Parse(startAmountTextbox.Text);
+            }
+            else {
+                bet = previousBet * 2;
+            }
+
+            betStreak++;
+            previousBet = bet;
+            textboxElement.Value = bet.ToString();
+
+            if (colourCombo.Text == "red")
+            {
+                redButton.Click();
+            }
+            else {
+                blackButton.Click();
+            }
+        }
+
+        private bool checkIfWon()
+        {
+            string wonColor = getBetResult();
+            string selectedColor = colourCombo.Text;
+
+            if (wonColor == selectedColor)
+            {
+                //We Won
+                concurrentWins++;
+                concurrentLosses = 0;
+                wins++;
+                betStreak = 0;
+                return true;
+
+            }
+            else {
+                //We Lost
+                concurrentWins = 0;
+                concurrentLosses++;
+                return false;
+            }
+
+        }
+
+        public string getBetResult()
+        {
+            GeckoElement lastChild = (GeckoElement)resultList.LastChild;
+            string color = lastChild.GetAttribute("data-color");
+            return color;
+
+        }
+
+        private void updateLabels()
+        {
+            getCredits();
+
+            winsLabel.Text = "Wins: " + wins;
+            concurLossLabel.Text = "Concurrent losses: " + concurrentLosses;
+            concurWinLabel.Text = "Concurrent wins: " + concurrentWins;
+
+        }
+
+        private void setStopNext(object sender, System.EventArgs e)
+        {
+            stopNext = stopCheckbox.Checked;
+        }
+    }
+}
